39e73f2d59d5b6c09edff982a4121c63a3a55fcc
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-25 09:32:51 +0000

    first test passing

---
+++ spec/gilded_rose_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/gilded_rose'
+require './lib/item'
+
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "foo"
+    end
+  end
+
+end
