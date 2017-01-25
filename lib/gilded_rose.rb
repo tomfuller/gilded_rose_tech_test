98601eca8a9023e3a66dcd3b6566e70c16938dd0
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-25 12:01:06 +0000

    tests pass, refactored and added functionality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,54 +1,99 @@
 
 class GildedRose
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      sort_all_items(item)
     end
   end
+
+  private
+
+  def item_special?(item)
+    aged_brie?(item) || backstage_passes?(item) || item_conjured?(item)
+  end
+
+  def sort_all_items(item)
+    item.sell_in -= 1 unless sulfuras?(item)
+    sort_normal_quality(item) unless item_special?(item)
+    sort_special_items(item) if item_special?(item)
+  end
+
+  def sort_special_items(item)
+    sort_brie_passes_and_conjured(item)
+    zero_sell_in(item) if item_sell_in_zero?(item)
+  end
+
+  def sort_brie_passes_and_conjured(item)
+    sort_brie_quality(item) if aged_brie?(item)
+    item.quality += backstage_passes_sell_in_to_quality(item) if backstage_passes?(item)
+    sort_conjured_quality(item) if item_conjured?(item)
+  end
+
+  def sort_conjured_quality(item)
+    item.quality -= 2 if item_sell_in_zero?(item)
+    item.quality -= 2
+  end
+
+  def sort_normal_quality(item)
+    subtract_1_quality(item) unless sulfuras?(item)
+    subtract_1_quality(item) if item_sell_in_zero?(item)
+  end
+
+  def item_conjured?(item)
+    item.name == 'Conjured'
+  end
+
+  def item_sell_in_zero?(item)
+    item.sell_in <= 0
+  end
+
+  def zero_sell_in(item)
+    backstage_pass_quality_to_0(item) if backstage_passes?(item)
+    subtract_1_quality(item) unless sulfuras?(item) || item_conjured?(item)
+  end
+
+  def aged_brie?(item)
+    item.name == 'Aged Brie'
+  end
+
+  def backstage_passes?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def sulfuras?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def add_1_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def subtract_1_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
+
+  def sort_brie_quality(item)
+    item.quality += 2 if item.quality < 50 && item_sell_in_zero?(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def backstage_pass_quality_to_0(item)
+    item.quality = 0
+  end
+
+  def quality_greater_than_0(item)
+    item.quality > 0
+  end
+
+  def backstage_passes_sell_in_to_quality(item)
+    return 3 if item.sell_in < 6
+    return 2 if item.sell_in < 11
+    1
+  end
+
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,11 +6,124 @@ require './lib/item'
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    context 'normal items' do
+
+      it 'should lose 1 quality if normal item' do
+        items = [Item.new("foo", 3, 3)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'should lose 2 quality if sell by date is gone on normal item' do
+        items = [Item.new("foo", 0, 3)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'should not lose quality if quality is already 0' do
+        items = [Item.new("foo", 0, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+      it 'should have a decreasing sell_in value' do
+        items = [Item.new("foo", 5, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 4
+      end
+
+    end
+
+    context 'sulfuras' do
+
+      it 'should do nothing to values of sulfuras' do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 2, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+        expect(items[0].sell_in).to eq 2
+      end
+
+    end
+
+    context 'aged brie' do
+
+      it 'should gain 1 quality if aged brie still in sell by' do
+        items = [Item.new("Aged Brie", 3, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'should gain 2 quality if aged brie out of sell by' do
+        items = [Item.new("Aged Brie", 0, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'should cap quality at 50 of aged brie' do
+        items = [Item.new("Aged Brie", 2, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+
+    end
+
+    context 'backstage passes' do
+
+      it 'should cap quality at 50 of backstage passes' do
+        items = [Item.new("Aged Brie", 2, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+
+      it 'should increase quality by 1 if sell in >10 of backstage passes' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 5
+      end
+
+      it 'should increase quality by 2 if sell in <10 of backstage passes' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 6
+      end
+
+      it 'should increase quality by 3 if sell in <5 of backstage passes' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 7
+      end
+
+      it 'should drop quality to 0 once sell in is 0 of backstage passes' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+    end
+
+    context 'conjured items' do
+
+      it 'should lose 2 quality when conjured item sell_in is greater than 0' do
+        items = [Item.new("Conjured", 7, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'should lose 4 quality when conjured item sell_in is less than 0' do
+        items = [Item.new("Conjured", 0, 6)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+      end
+
+    end
+
   end
 
 end
